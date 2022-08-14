53662d3c32cfbbddfa9db9e43740fab5d8262193
ChoiByeongMin <coqoa28@gmail.com>
2022-08-14 18:59:37 +0900

    220814 BuildContext 이해하기(17,18,19강)
    Snack Bar,
    Scaffold.of(),
    Build(),
    fluttertoast

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,105 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title : 'AppBar',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
+      title: 'Toast',
       home: MyPage(),
     );
   }
 }
-
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBar Icon Menu'),
+        title: Text('Snack Bar'),
         centerTitle: true,
-        elevation: 0.0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart), 
-            onPressed: () { 
-              print('쇼핑카트 터치 했음'); 
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search), 
-            onPressed: () { 
-              print('검색 터치 했음'); 
-            },
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/COQOA.jpeg'),
-                backgroundColor: Colors.white,
-                
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/POMOTODO.png'),
-                  backgroundColor: Colors.white,
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage('assets/POMOTODO.png'),
-                //   backgroundColor: Colors.white,
-                // ),
-              ],
-
-              accountName: Text('최병민'),
-              accountEmail: Text('coqoa28@gmail.com'),
-              onDetailsPressed:(){
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.orange[500],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0)
-                )
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home,
-                color: Colors.grey[800],
-              ),
-              title: Text('Home'),
-              onTap: (){
-                print('Home is Clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings,
-                color: Colors.grey[800],
-              ),
-              title: Text('Settings'),
-              onTap: (){
-                print('Settings is Clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.question_answer,
-                color: Colors.grey[800],
-              ),
-              title: Text('Q&A'),
-              onTap: (){
-                print('Q&A is Clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
+      body: Center(
+        child: FlatButton(
+          onPressed:(){
+            flutterToast();
+          },
+          child: Text('Toast'), 
+          color: Colors.blue,
         ),
       ),
     );
   }
 }
+
+void flutterToast(){
+  Fluttertoast.showToast(msg: "Flutterrrrr",
+    // 커스터마이징
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: Colors.redAccent,
+    fontSize: 20.0,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT
+  );
+}
+
