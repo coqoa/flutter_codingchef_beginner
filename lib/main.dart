9b6fdb13f1bd3292b247584d772b3fb069770019
ChoiByeongMin <coqoa28@gmail.com>
2022-08-14 15:12:42 +0900

    220814 Drawer 메뉴 만들기  (15,16강)

--- lib/main.dart
+++ lib/main.dart
@@ -23,26 +23,87 @@ class MyPage extends StatelessWidget {
         title: Text('AppBar Icon Menu'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu), 
-          onPressed: () { 
-            print('메뉴 클릭 했음'); 
-          },
-        ),
         actions: [
           IconButton(
             icon: Icon(Icons.shopping_cart), 
             onPressed: () { 
-              print('쇼핑카트 클릭 했음'); 
+              print('쇼핑카트 터치 했음'); 
             },
           ),
           IconButton(
             icon: Icon(Icons.search), 
             onPressed: () { 
-              print('검색 클릭 했음'); 
+              print('검색 터치 했음'); 
             },
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/COQOA.jpeg'),
+                backgroundColor: Colors.white,
+                
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/POMOTODO.png'),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage('assets/POMOTODO.png'),
+                //   backgroundColor: Colors.white,
+                // ),
+              ],
+
+              accountName: Text('최병민'),
+              accountEmail: Text('coqoa28@gmail.com'),
+              onDetailsPressed:(){
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.orange[500],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0)
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+                color: Colors.grey[800],
+              ),
+              title: Text('Home'),
+              onTap: (){
+                print('Home is Clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                color: Colors.grey[800],
+              ),
+              title: Text('Settings'),
+              onTap: (){
+                print('Settings is Clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                color: Colors.grey[800],
+              ),
+              title: Text('Q&A'),
+              onTap: (){
+                print('Q&A is Clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
