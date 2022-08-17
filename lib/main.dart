f5c266bc00a8c878866b7617cdf46ba7063c7882
ChoiByeongMin <coqoa28@gmail.com>
2022-08-17 23:13:36 +0900

    220817 StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,61 +4,36 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyPage(),
-    );
-  }
+  State<MyApp> createState() => MyAppState();
 }
-// class MyPage extends StatelessWidget {
-//   const MyPage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('AppBar'),
-//       ),
-//       body: ,
-//     );
-//   }
-// }
-class MyPage extends StatefulWidget {
-  const MyPage({Key? key}) : super(key: key);
-
-  @override
-  State<MyPage> createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
+// MyApp를 상속받는 클래스
+class MyAppState extends State<MyApp> {
   int count = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('AppBar')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Count : ',
-              style: TextStyle(fontSize: 30.0),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(fontSize: 40.0, color: Colors.pink),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    count++;
-                  });
-                },
-                child: Text('BUTTON'))
-          ],
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Stateful Widget')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('clickBtn'),
+              Text('$count')
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            setState(() {
+              count++;
+              print(count);
+            });
+          },
         ),
       ),
     );
