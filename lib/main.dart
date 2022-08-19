978222cbd511591b81a8f76aa16117ad265e85f2
ChoiByeongMin <coqoa28@gmail.com>
2022-08-19 23:44:47 +0900

    220819 기본 로그인 페이지

--- lib/main.dart
+++ lib/main.dart
@@ -9,155 +9,80 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ListViewPage(),
-    );
+    return const MaterialApp(title: 'Dice game', home: LogIn());
   }
 }
 
-class ListViewPage extends StatefulWidget {
-  const ListViewPage({Key? key}) : super(key: key);
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
 
   @override
-  State<ListViewPage> createState() => _ListViewPageState();
+  State<LogIn> createState() => _LogInState();
 }
 
-class _ListViewPageState extends State<ListViewPage> {
-  var titleList = [
-    'COQOA',
-    'Heart',
-    'JAVADOT',
-    'POMOTODO',
-    'Star',
-    'TAPTAP',
-  ];
-  var imageList = [
-    'assets/COQOA.jpeg',
-    'assets/Heart.png',
-    'assets/JAVADOT.png',
-    'assets/POMOTODO.png',
-    'assets/Star.png',
-    'assets/TAPTAP.png',
-  ];
-  var description = [
-    '1. description 1 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-    '2. description 2 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-    '3. description 3 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-    '4. description 4 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-    '5. description 5 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-    '6. description 6 is long long long.. long long long.. long long long.. long long long.. long long long.. long long long.. long long long..',
-  ];
-  void showPopUp(context, title, image, description) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 380,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.white),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(image, width: 200, height: 200),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      description,
-                      maxLines: 3,
-                      style: TextStyle(fontSize: 15, color: Colors.grey[500]),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.close), 
-                    label: const Text('Close'),
-                    ),
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width * 0.6;
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'ListView',
-          style: TextStyle(color: Colors.grey),
+        title: Text('Log In'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
         ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
       ),
-      body: ListView.builder(
-        itemCount: titleList.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              print(titleList[index]);
-              showPopUp(context, titleList[index], imageList[index],
-                  description[index]);
-            },
-            child: Card(
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset(imageList[index]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Text(
-                          titleList[index],
-                          style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                          width: width,
-                          child: Text(
-                            description[index],
-                            style: TextStyle(
-                                fontSize: 15, color: Colors.grey[500]),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        //
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50.0)),
+            Center(
+              child: Image(
+                image: AssetImage('assets/COQOA.jpeg'),
+                width: 170.0,
+                height: 190.0,
               ),
             ),
-          );
-        },
+            Form(
+                child: Theme(
+                    data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: InputDecorationTheme(
+                            labelStyle:
+                                TextStyle(color: Colors.teal, fontSize: 15.0))),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration:
+                                InputDecoration(labelText: 'Enter Dice'),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            decoration:
+                                InputDecoration(labelText: 'Enter Password'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 40.0,),
+                          ButtonTheme(
+                              minWidth: 100.0,
+                              height: 50.0,
+                              child: RaisedButton(
+                                  color: Colors.orangeAccent,
+                                  child: Icon(Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 35.0,),
+                                  onPressed: () {}))
+                        ],
+                      ),
+                    ))),
+          ],
+        ),
       ),
     );
   }
