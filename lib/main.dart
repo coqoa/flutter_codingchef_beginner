129a64daea490cbe8e67d63951d19f40fc5072b8
ChoiByeongMin <coqoa28@gmail.com>
2022-08-20 00:31:42 +0900

    220820 if문을 통한 ID검증, Snackbar 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dice.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -21,6 +23,9 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,56 +39,111 @@ class _LogInState extends State<LogIn> {
         ),
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
       ),
-      body: SingleChildScrollView(
-        //
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50.0)),
-            Center(
-              child: Image(
-                image: AssetImage('assets/COQOA.jpeg'),
-                width: 170.0,
-                height: 190.0,
+      body: Builder(builder: (context) {
+        return SingleChildScrollView(
+          //
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50.0)),
+              Center(
+                child: Image(
+                  image: AssetImage('assets/COQOA.jpeg'),
+                  width: 170.0,
+                  height: 190.0,
+                ),
               ),
-            ),
-            Form(
-                child: Theme(
-                    data: ThemeData(
-                        primaryColor: Colors.teal,
-                        inputDecorationTheme: InputDecorationTheme(
-                            labelStyle:
-                                TextStyle(color: Colors.teal, fontSize: 15.0))),
-                    child: Container(
-                      padding: EdgeInsets.all(40.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Enter Dice'),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Enter Password'),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          SizedBox(height: 40.0,),
-                          ButtonTheme(
-                              minWidth: 100.0,
-                              height: 50.0,
-                              child: RaisedButton(
-                                  color: Colors.orangeAccent,
-                                  child: Icon(Icons.arrow_forward,
-                                  color: Colors.white,
-                                  size: 35.0,),
-                                  onPressed: () {}))
-                        ],
-                      ),
-                    ))),
-          ],
-        ),
-      ),
+              Form(
+                  child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle: TextStyle(
+                                  color: Colors.teal, fontSize: 15.0))),
+                      child: Container(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration:
+                                  InputDecoration(labelText: 'Enter ID'),
+                              keyboardType: TextInputType.emailAddress,
+                              controller: controller,
+                            ),
+                            TextField(
+                              decoration:
+                                  InputDecoration(labelText: 'Enter Password'),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              controller: controller2,
+                            ),
+                            SizedBox(
+                              height: 40.0,
+                            ),
+                            ButtonTheme(
+                                minWidth: 100.0,
+                                height: 50.0,
+                                child: RaisedButton(
+                                    color: Colors.orangeAccent,
+                                    child: Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.white,
+                                      size: 35.0,
+                                    ),
+                                    onPressed: () {
+                                      if (controller.text == 'admin' &&
+                                          controller2.text == '1234') {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => Dice()));
+                                      } else if (controller.text == 'admin' &&
+                                          controller2.text != '1234') {
+                                            showSnackBar(context);
+                                      } else if (controller.text != 'admin' &&
+                                          controller2.text == '1234') {
+                                            showSnackBar2(context);
+                                      } else {
+                                        showSnackBar3(context);
+                                      }
+                                    }))
+                          ],
+                        ),
+                      ))),
+            ],
+          ),
+        );
+      }),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '비밀번호가 일치하지 않습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '아이디가 일치하지 않습니다.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+void showSnackBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text(
+      '아이디와 비밀번호 모두 틀렸습니다',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
