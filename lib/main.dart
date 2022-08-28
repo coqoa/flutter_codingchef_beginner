1ffb9aeed8f8887701c9d1e1c1d66e368648fa7b
ChoiByeongMin <coqoa28@gmail.com>
2022-08-28 22:08:18 +0900

    220828 Life Cycle 예제

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,98 @@
 
 // ▲ 매운맛 수강 코드 ▲
 
+// Life Cycle 예제
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'MyApp',
+//       home: ScreenA(),
+//     );
+//   }
+// }
+
+// class ScreenA extends StatelessWidget {
+//   const ScreenA({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('ScreenA'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(context,
+//                     MaterialPageRoute(builder: (context) => ScreenB()));
+//               },
+//               child: Text('GO TO SCREEN B'),
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 print('c');
+//               },
+//               child: Text('GO TO SCREEN C'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class ScreenB extends StatefulWidget {
+//   const ScreenB({Key? key}) : super(key: key);
+
+//   @override
+//   State<ScreenB> createState() => _ScreenBState();
+// }
+
+// class _ScreenBState extends State<ScreenB> {
+//   @override
+//   void initState() {
+//     // Stateful Widget을 생성해서 Widget Tree에 삽입되자마자 initState()가호출된다
+//     super.initState();
+//     print('----- Screen B InitState is Called -----');
+//   }
+
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     super.dispose();
+//     print('----- Screen B Dispose is Called -----');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print('----- Screen B Build is called -----');
+//     return Scaffold(
+//         appBar: AppBar(title: Text('ScreenB')),
+//         body: Center(
+//             child: ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                 },
+//                 child: Text('POP!'))));
+//   }
+// }
+
+// weathreApp
 import 'package:flutter/material.dart';
+import 'package:flutter_codingchef_beginner/screens/loading.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -135,153 +226,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListViewPage(),
-    );
-  }
-}
-
-class ListViewPage extends StatefulWidget {
-  const ListViewPage({Key? key}) : super(key: key);
-
-  @override
-  State<ListViewPage> createState() => _ListViewPageState();
-}
-
-class _ListViewPageState extends State<ListViewPage> {
-  var titleList = [
-    'COQOA',
-    'GIT',
-    'HEART',
-    'IAMPORT',
-    'JAVADOT',
-    'POMOTODO',
-    'STAR',
-    'TAPTAP',
-  ];
-  var imageList = [
-    'assets/COQOA.jpeg',
-    'assets/GIT.png',
-    'assets/Heart.png',
-    'assets/IAMPORT.png',
-    'assets/JAVADOT.png',
-    'assets/POMOTODO.png',
-    'assets/Star.png',
-    'assets/TAPTAP.png',
-  ];
-  var description = [
-    '1. description description description description description description description',
-    '2. description description description description description description description',
-    '3. description description description description description description description',
-    '4. description description description description description description description',
-    '5. description description description description description description description',
-    '6. description description description description description description description',
-    '7. description description description description description description description',
-    '8. description description description description description description description',
-  ];
-  void showPopup(context, title, image, description) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 380,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.white),
-              child: Column(
-                children: [
-                  SizedBox(height: 10),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(image, width: 200, height: 200),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      description,
-                      maxLines: 3,
-                      style: TextStyle(fontSize: 15, color: Colors.grey[500]),
-                    ),
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.close),
-                    label: const Text('close'),
-                  )
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width * 0.6;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListView', style: TextStyle(color: Colors.grey)),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: ListView.builder(
-          itemCount: titleList.length,
-          itemBuilder: (context, index) {
-            // onTap을 위한 GestureDetector
-            return GestureDetector(
-              onTap: () {
-                showPopup(context, titleList[index], imageList[index],
-                    description[index]);
-              },
-              child: Card(
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 100,
-                      child: Image.asset(imageList[index]),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          Text(
-                            titleList[index],
-                            style: const TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey),
-                          ),
-                          SizedBox(height: 10),
-                          SizedBox(
-                            width: width,
-                            child: Text(
-                              description[index],
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey[500]),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }),
+      title: 'MyApp',
+      home: Loading(),
     );
   }
 }
