f39ec5c6ffc690e6dd1e09514995da1dd44c9b5a
ChoiByeongMin <coqoa28@gmail.com>
2022-08-28 22:29:56 +0900

    220828 geolocator를 이용한 주소출력하기

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,13 +9,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getLocation()async{
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(onPressed: () {
-
-        }, child: Text('Get My Location'),style: ElevatedButton.styleFrom(primary: Colors.redAccent)),
+        child: ElevatedButton(
+            onPressed: () {
+              getLocation();
+            },
+            child: Text('Get My Location'),
+            style: ElevatedButton.styleFrom(primary: Colors.redAccent)),
       ),
     );
   }
