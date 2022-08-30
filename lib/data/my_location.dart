16e6ef2e4741742f969e593e841abc06e9970684
ChoiByeongMin <coqoa28@gmail.com>
2022-08-30 23:36:19 +0900

    220830 날씨 앱 만들기

--- lib/data/my_location.dart
+++ lib/data/my_location.dart
@@ -1,22 +1,19 @@
-import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator/geolocator.dart';
 
-class MyLocation{
-  double? latitude2;
-  double? longitude2;
+// class MyLocation{
+//   double latitude2;
+//   double longitude2;
 
-  Future<void> getMyCurrentLocation()async{
-    try{
-      LocationPermission permission = await Geolocator.requestPermission();
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      latitude2 = position.latitude;
-      longitude2 = position.longitude;
-      // print('location = ${position}');
-      // print('latitude = $latitude2');
-      // print('longitude = $longitude2');
-    }
-    catch(e){
-      print(e);
-      print('문제가 발생했습니다');
-    }
-  }
-}
+//   Future<void> getMyCurrentLocation() async{
+//     try {
+//       Position position =  await Geolocator.
+//       getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+//       latitude2 = position.latitude;
+//       longitude2 = position.longitude;
+//       print(latitude2);
+//       print(longitude2);
+//     }catch(e){
+//       print('There was a problem with the internet connection.');
+//     }
+//   }
+// }
--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,81 +1,82 @@
-import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:lecture/data/my_location.dart';
+// import 'package:lecture/data/network.dart';
+// import 'package:lecture/screens/weather_screen.dart';
 
-import 'package:flutter_codingchef_beginner/data/my_location.dart';
-import 'package:flutter_codingchef_beginner/data/network.dart';
+// const apiKey = '0d0cc1131b44cd6ea0027e60e69dc007';
 
-class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
+// class Loading extends StatefulWidget {
+//   @override
+//   _LoadingState createState() => _LoadingState();
+// }
 
-  @override
-  State<Loading> createState() => _LoadingState();
-}
+// class _LoadingState extends State<Loading> {
+//   double latitude3;
+//   double longitude3;
 
-class _LoadingState extends State<Loading> {
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     getLocation();
+//   }
 
-  double? latitude3;
-  double? longitude3;
+//   void getLocation() async {
+//     MyLocation myLocation = MyLocation();
+//     await myLocation.getMyCurrentLocation();
+//     latitude3 = myLocation.latitude2;
+//     longitude3 = myLocation.longitude2;
+//     print(latitude3);
+//     print(longitude3);
 
-  void getLocation()async{
-    // MyLocation().getMyCurrentLocation();
-    MyLocation myLocation = MyLocation();
-    await myLocation.getMyCurrentLocation();
-    latitude3=myLocation.latitude2;
-    longitude3=myLocation.longitude2;
-    print(latitude3);
-    print(longitude3);
-    // my_location 파일로 옮김
-    // try{
-    //   LocationPermission permission = await Geolocator.requestPermission();
-    //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    //   latitude2 = position.latitude;
-    //   longitude2 = position.longitude;
-    //   print('location = ${position}');
-    //   print('latitude = $latitude2');
-    //   print('longitude = $longitude2');
-    // }
-    // catch(e){
-    //   print(e);
-    //   print('문제가 발생했습니다');
-    Network network = Network('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
-    
-    var weatherData = await network.getJsonData();
-    print(weatherData);
-    }
+//     Network network = Network(
+//         'https://api.openweathermap.org/data/2.5/weather'
+//             '?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric',
+//         'https://api.openweathermap.org/data/2.5/air_pollution'
+//             '?lat=$latitude3&lon=$longitude3&appid=$apiKey');
 
-  // void fetchData()async{
-  //   // http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
-  //   // if(response.statusCode == 200){
-  //   //   String jsonData = response.body;
-  //   //   var parsingData = jsonDecode(jsonData);
-  //     // print('weather description = ${parsingData["weather"][0]["description"]}');
-  //     // print('wind speed = ${parsingData["wind"]["speed"]}');
-  //     // print('id = ${parsingData["id"]}');
-  //   // }else{
-  //   //   print(response.statusCode);
-  //   // }
-  // }
+//     var weatherData = await network.getJsonData();
+//     print(weatherData);
 
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
-    // fetchData();
-  }
+//     var airData = await network.getAirData();
+//     print(airData);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              // getLocation();
-            },
-            child: Text('Get My Location'),
-            style: ElevatedButton.styleFrom(primary: Colors.redAccent)),
-      ),
-    );
-  }
-}
+//     Navigator.push(context, MaterialPageRoute(builder: (context) {
+//       return WeatherScreen(
+//         parseWeatherData: weatherData,
+//         parseAirPollution: airData,
+//       );
+//     }));
+//   }
+
+//   // void fetchData() async{
+//   //
+//   //     var myJson = parsingData['weather'][0]['description'];
+//   //     print(myJson);
+//   //
+//   //     var wind = parsingData['wind']['speed'];
+//   //     print(wind);
+//   //
+//   //     var id =parsingData['id'];
+//   //     print(id);
+//   //   }else{
+//   //     print(response.statusCode);
+//   //   }
+//   // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: RaisedButton(
+//           onPressed: null,
+//           child: Text(
+//             'Get my location',
+//             style: TextStyle(color: Colors.white),
+//           ),
+//           color: Colors.blue,
+//         ),
+//       ),
+//     );
+//   }
+// }
